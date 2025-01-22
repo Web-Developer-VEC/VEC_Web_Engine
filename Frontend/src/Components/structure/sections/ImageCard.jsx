3e6ab8318a96263bfab586e244221fbf3f9d13e7
SriHari22004 <srihari22004@gmail.com>
2025-01-22 15:27:20 +0530

    Faculties Test

--- Frontend/src/Components/structure/sections/ImageCard.jsx
+++ Frontend/src/Components/structure/sections/ImageCard.jsx
@@ -3,30 +3,31 @@ import { SiPublons } from "react-icons/si";
 import { FaGoogleScholar } from "react-icons/fa6";
 import { FaOrcid } from "react-icons/fa";
 import { FaResearchgate } from "react-icons/fa6";
-import styles from './Facullties.module.css';
+import styles from './Faculties.module.css';
 
 
-const ImageCard = ({ name, photo, Designation }) => (
+const ImageCard = ({ name, photo, Designation, Scholar, Research, Orchid, Publon, Scopus, Linkedin }) => (
   <div className={styles.imagecard}>
     <img src={photo} alt={name} />
     <h3>{name}</h3>
     <p>{Designation}</p>
     <div className={styles.socialLinks}>
-      <a href="" className={styles.socialLink}>
+      <a href={Publon} target="_blank" className={styles.socialLink}>
         <SiPublons />
       </a>
-      <a href="" className={styles.socialLink}>
+      <a href={Scholar} target="_blank" className={styles.socialLink}>
         <FaGoogleScholar />
       </a>
-      <a href="" className={styles.socialLink}>
+      <a href={Orchid} target="_blank" className={styles.socialLink}>
         <FaOrcid/>
      </a>
-     <a href="" className={styles.socialLink}>
+     <a href={Research} target="_blank" className={styles.socialLink}>
         <FaResearchgate/>
      </a>
      </div>
-    <button className='FACButton'>View More</button>
+    <button className={styles.FACButton}>View More</button>
   </div>
 );
 
 export default ImageCard;
+
