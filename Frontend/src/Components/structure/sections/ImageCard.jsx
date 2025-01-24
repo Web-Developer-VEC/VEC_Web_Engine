f0a3c5ef625f4818c0cb940ed7b3c00a5d8aaf28
SriHari22004 <srihari22004@gmail.com>
2025-01-24 11:26:32 +0530

    Faculty Page Integrated
    From db

--- Frontend/src/Components/structure/sections/ImageCard.jsx
+++ Frontend/src/Components/structure/sections/ImageCard.jsx
@@ -1,33 +1,48 @@
-import React from 'react';
+import React from "react";
 import { SiPublons } from "react-icons/si";
 import { FaGoogleScholar } from "react-icons/fa6";
-import { FaOrcid } from "react-icons/fa";
-import { FaResearchgate } from "react-icons/fa6";
-import styles from './Faculties.module.css';
-
+import { FaOrcid, FaResearchgate, FaLinkedin } from "react-icons/fa";
+import styles from "./Faculties.module.css";
 
 const ImageCard = ({ name, photo, Designation, Scholar, Research, Orchid, Publon, Scopus, Linkedin }) => (
   <div className={styles.imagecard}>
     <img src={photo} alt={name} />
     <h3>{name}</h3>
-    <p>{Designation}</p>
+    <p className={styles.imagecard_p}>{Designation}</p>
     <div className={styles.socialLinks}>
-      <a href={Publon} target="_blank" className={styles.socialLink}>
-        <SiPublons />
-      </a>
-      <a href={Scholar} target="_blank" className={styles.socialLink}>
-        <FaGoogleScholar />
-      </a>
-      <a href={Orchid} target="_blank" className={styles.socialLink}>
-        <FaOrcid/>
-     </a>
-     <a href={Research} target="_blank" className={styles.socialLink}>
-        <FaResearchgate/>
-     </a>
-     </div>
+      {Publon && (
+        <a href={Publon} target="_blank" rel="noopener noreferrer" className={styles.socialLink}>
+          <SiPublons />
+        </a>
+      )}
+      {Scholar && (
+        <a href={Scholar} target="_blank" rel="noopener noreferrer" className={styles.socialLink}>
+          <FaGoogleScholar />
+        </a>
+      )}
+      {Orchid && (
+        <a href={Orchid} target="_blank" rel="noopener noreferrer" className={styles.socialLink}>
+          <FaOrcid />
+        </a>
+      )}
+      {Research && (
+        <a href={Research} target="_blank" rel="noopener noreferrer" className={styles.socialLink}>
+          <FaResearchgate />
+        </a>
+      )}
+      {Linkedin && (
+        <a href={Linkedin} target="_blank" rel="noopener noreferrer" className={styles.socialLink}>
+          <FaLinkedin />
+        </a>
+      )}
+      {Scopus && (
+        <a href={Scopus} target="_blank" rel="noopener noreferrer" className={styles.socialLink}>
+          <img src="/Images/scopus.png" alt="Scopus Author Profile" />
+        </a>
+      )}
+    </div>
     <button className={styles.FACButton}>View More</button>
   </div>
 );
 
 export default ImageCard;
-
