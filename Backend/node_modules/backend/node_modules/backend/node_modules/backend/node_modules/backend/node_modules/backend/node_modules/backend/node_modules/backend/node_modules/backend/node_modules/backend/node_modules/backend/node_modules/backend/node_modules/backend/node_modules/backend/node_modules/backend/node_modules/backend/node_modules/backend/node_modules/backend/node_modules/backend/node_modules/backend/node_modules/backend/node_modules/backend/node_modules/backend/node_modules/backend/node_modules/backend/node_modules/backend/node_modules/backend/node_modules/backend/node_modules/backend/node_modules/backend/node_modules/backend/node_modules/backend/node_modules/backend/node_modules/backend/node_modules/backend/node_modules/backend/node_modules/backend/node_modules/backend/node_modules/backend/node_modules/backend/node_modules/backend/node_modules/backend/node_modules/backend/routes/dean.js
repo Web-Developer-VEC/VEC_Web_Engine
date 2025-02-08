7ce0781d734664d24a978d2546e659df00a2c6e3
Leeeroy <90497821+Shadow-Knight503@users.noreply.github.com>
2025-02-08 13:31:36 +0530

    Delete Backend/node_modules Fork Up

--- Backend/node_modules/backend/node_modules/backend/node_modules/backend/node_modules/backend/node_modules/backend/node_modules/backend/node_modules/backend/node_modules/backend/node_modules/backend/node_modules/backend/node_modules/backend/node_modules/backend/node_modules/backend/node_modules/backend/node_modules/backend/node_modules/backend/node_modules/backend/node_modules/backend/node_modules/backend/node_modules/backend/node_modules/backend/node_modules/backend/node_modules/backend/node_modules/backend/node_modules/backend/node_modules/backend/node_modules/backend/node_modules/backend/node_modules/backend/node_modules/backend/node_modules/backend/node_modules/backend/node_modules/backend/node_modules/backend/node_modules/backend/node_modules/backend/node_modules/backend/node_modules/backend/node_modules/backend/node_modules/backend/node_modules/backend/node_modules/backend/routes/dean.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { getDean } = require('../controllers/deanController');
-
-const router = express.Router();
-
-router.use('/deanandassociates', getDean);
-
-module.exports = router
--- Backend/node_modules/backend/node_modules/backend/node_modules/backend/node_modules/backend/node_modules/backend/node_modules/backend/node_modules/backend/node_modules/backend/node_modules/backend/node_modules/backend/node_modules/backend/node_modules/backend/node_modules/backend/node_modules/backend/node_modules/backend/node_modules/backend/node_modules/backend/node_modules/backend/node_modules/backend/node_modules/backend/node_modules/backend/node_modules/backend/node_modules/backend/node_modules/backend/routes/committee.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { getCommittee } = require('../controllers/committeeController');
-
-
-const router = express.Router();
-
-router.get('/committee', getCommittee);
-
-module.exports = router;
