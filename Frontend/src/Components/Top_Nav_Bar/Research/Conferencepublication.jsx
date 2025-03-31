eec6fbe8b4e95541881f146f712266f48c4c8879
SATHISH KUMAR <sathishkumar290705@gmail.com>
2025-03-31 21:00:59 +0530

    conferencepublication, journalpublication
    conference publication and journal publication

--- Frontend/src/Components/Top_Nav_Bar/Research/Conferencepublication.jsx
+++ Frontend/src/Components/Top_Nav_Bar/Research/Conferencepublication.jsx
@@ -1,34 +1,59 @@
 import { useEffect, useState } from "react";
 import "./Conferencepublication.css";
 import Banner from "../../Banner";
+import axios from "axios";
+import Reseachdetails from "./reseach details";
+const dept = {
+  courses : [
+    "B.E Automobile Engineering",
+    "B.E Civil Engineering",
+    "B.E Computer Science and Engineering",
+    "B.E Computer Science and Engineering (Cyber Security)",
+    "B.E Electronics and Communication Engineering",
+    "B.E Electrical and Electronics Engineering",
+    "B.E Electronics and Instrumentation Engineering",
+    "B.E Mechanical Engineering",
+    "B.Tech Artificial Intelligence and Data Science",
+    "B.Tech Information Technology",
+    "M.E Computer Science Engineering",
+    "M.E Power System Engineering",
+    "M.B.A Master of Business Administration",
+  ],
 
-const courses = [
-  "B.E Automobile Engineering",
-  "B.E Civil Engineering",
-  "B.E Computer Science and Engineering",
-  "B.E Computer Science and Engineering (Cyber Security)",
-  "B.E Electronics and Communication Engineering",
-  "B.E Electrical and Electronics Engineering",
-  "B.E Electronics and Instrumentation Engineering",
-  "B.E Mechanical Engineering",
-  "B.Tech Artificial Intelligence and Data Science",
-  "B.Tech Information Technology",
-  "M.E Computer Science Engineering",
-  "M.E Power System Engineering",
-  "M.B.A Master of Business Administration",
-];
+  dept_id : [
+    "002",
+    "004",
+    "005",
+    "006",
+    "009",
+    "007",
+    "008",
+    "013",
+    "001",
+    "011",
+    "005",
+    "007",
+    "017"
+  ]
+};
 
 export default function Conferencepublication({theme, toggle}) {
   const [pdfUrl, setPdfUrl] = useState(null); // Default PDF
   const [activeYear, setActiveYear] = useState(null); // Track active button
   const [conference, setConference] = useState(null);
-
+  const [open,setopen]= useState(false)
+  const [course , setcourse]= useState(null);
+  const [deptid,setdeptid] = useState(null);
+  const [conferenceid , setconferenceid] = useState(null)
   const BASE_URL = process.env.REACT_APP_BASE_URL;
-
   const [selectedCourse, setSelectedCourse] = useState(null);
-
-  const handleCourseClick = (course) => {
+  const handleCourseClick = (course,id) => {
+    setcourse(course);
+    setdeptid(id)
+    const conferencecontent = conference.filter(conference_dept => conference_dept.dept_id === id )
+   setconferenceid(conferencecontent)
     setSelectedCourse(selectedCourse === course ? null : course);
+    setopen(true);
   };
 
   const UrlParser = (path) => {
@@ -38,26 +63,16 @@ export default function Conferencepublication({theme, toggle}) {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch("/api/get_research_data", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({ category: "conference_publication" }),
-        });
-  
-        const data = await response.json();
-  
-        if (response.ok) {
-          setConference(data);
-          setPdfUrl(data?.pdf_path[0]);
-          setActiveYear(data?.year[0]);
-        }
-      } catch (error) {
-        console.error("Fetching Error:", error);
+
+        const response = await axios.get('/api/conference')
+        setConference(response.data)
+        console.log(response.data)
+      }catch(error){
+        console.error("Fetching error",error)
       }
     }
-
+  
+  
     fetchData();
   }, []);
 
@@ -70,6 +85,16 @@ export default function Conferencepublication({theme, toggle}) {
           subHeaderText="Enrich Your Knowledge"
         />
       </div>
+
+    {open ? (
+      <>
+      <div className="ml-[3rem] mt-[1rem]">
+        <button onClick={()=>{setopen(false); setcourse(null); setdeptid(null)} } className="backbutton"> back </button>
+      
+      </div>
+      <Reseachdetails course={conference} data1={conferenceid}/>
+      </>
+    ):(
       <div className="research-conference-container">
         <h1 className="research-conference-title">
           Conference Publication
@@ -77,13 +102,13 @@ export default function Conferencepublication({theme, toggle}) {
 
         <div className="course-selection-container">
           
-          {courses.map((course) => (
+          {dept.courses.map((course,id) => (
             <div
               key={course}
               className={`course-card ${
                 selectedCourse === course ? "expanded" : ""
               }`}
-              onClick={() => handleCourseClick(course)}
+              onClick={() => handleCourseClick(course ,dept.dept_id[id])}
             >
               <div className="course-header">
                 <span className="icon"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-book-open course-icon w-10 text-lg text-secd dark:text-drks"><path d="M12 7v14"></path><path d="M3 18a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1h5a4 4 0 0 1 4 4 4 4 0 0 1 4-4h5a1 1 0 0 1 1 1v13a1 1 0 0 1-1 1h-6a3 3 0 0 0-3 3 3 3 0 0 0-3-3z"></path></svg></span>
@@ -93,6 +118,8 @@ export default function Conferencepublication({theme, toggle}) {
           ))}
         </div>
       </div>
+
+    )}
     </>
   );
 }
--- Frontend/src/Components/Top_Nav_Bar/Research/Journalpublication.jsx
+++ Frontend/src/Components/Top_Nav_Bar/Research/Journalpublication.jsx
@@ -1,65 +1,85 @@
 import { useEffect, useState } from "react";
 import "./Journalpublication.css";
 import Banner from "../../Banner";
+import axios from "axios";
+import Reseachdetails from "./reseach details";
 
-const courses = [
-  "B.E Automobile Engineering",
-  "B.E Civil Engineering",
-  "B.E Computer Science and Engineering",
-  "B.E Computer Science and Engineering (Cyber Security)",
-  "B.E Electronics and Communication Engineering",
-  "B.E Electrical and Electronics Engineering",
-  "B.E Electronics and Instrumentation Engineering",
-  "B.E Mechanical Engineering",
-  "B.Tech Artificial Intelligence and Data Science",
-  "B.Tech Information Technology",
-  "M.E Computer Science Engineering",
-  "M.E Power System Engineering",
-  "M.B.A Master of Business Administration",
-];
+const dept = {
+  courses : [
+    "B.E Automobile Engineering",
+    "B.E Civil Engineering",
+    "B.E Computer Science and Engineering",
+    "B.E Computer Science and Engineering (Cyber Security)",
+    "B.E Electronics and Communication Engineering",
+    "B.E Electrical and Electronics Engineering",
+    "B.E Electronics and Instrumentation Engineering",
+    "B.E Mechanical Engineering",
+    "B.Tech Artificial Intelligence and Data Science",
+    "B.Tech Information Technology",
+    "M.E Computer Science Engineering",
+    "M.E Power System Engineering",
+    "M.B.A Master of Business Administration",
+  ],
+
+  dept_id : [
+    "002",
+    "004",
+    "005",
+    "006",
+    "009",
+    "007",
+    "008",
+    "013",
+    "001",
+    "011",
+    "005",
+    "007",
+    "017"
+  ]
+};
 
 export default function Journalpublication({theme, toggle}) {
   const [pdfUrl, setPdfUrl] = useState(null); // Default PDF
   const [activeYear, setActiveYear] = useState(null); // Track active button
   const [journalPublication, setJournalPublication] = useState(null);
-
+  const [open,setopen] = useState(false);
+  const [journalPublicationid , setjournalPublicationid] = useState(null)
+  console.log("Ajith",journalPublicationid);
   const BASE_URL = process.env.REACT_APP_BASE_URL;
 
   const [selectedCourse, setSelectedCourse] = useState(null);
+  const [selectedDept,setSelectedDept] = useState(null);
 
-  const handleCourseClick = (course) => {
-    setSelectedCourse(selectedCourse === course ? null : course);
+  const handleCourseClick = (course,id) => {
+    setSelectedCourse(course);
+    setSelectedDept(id);  
+   
+    const journalcontent = journalPublication.filter(jour_dept => jour_dept.dept_id === id )
+    
+    setjournalPublicationid(journalcontent)
+ 
+    setopen(true);
+
   };
 
   const UrlParser = (path) => {
-  return path?.startsWith("http") ? path : `${BASE_URL}${path}`;
+    return path?.startsWith("http") ? path : `${BASE_URL}${path}`;
   };
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch("/api/get_research_data", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({ category: "journal_publication" }),
-        });
-  
-        const data = await response.json();
-  
-        if (response.ok) {
-          setJournalPublication(data);
-          setPdfUrl(data?.pdf_path[0]);
-          setActiveYear(data?.year[0]);
-        }
+        const response = await axios.get('/api/journal');
+        setJournalPublication(response.data)
+       console.log(journalPublication)
+        
       } catch (error) {
         console.error("Fetching Error:", error);
       }
     }
 
     fetchData();
-  }, []);
+  }, [journalPublication]);
 
   return (
     <>
@@ -70,20 +90,28 @@ export default function Journalpublication({theme, toggle}) {
           subHeaderText="Enrich Your Knowledge"
         />
       </div>
-      <div className="research-journal-container">
+
+      {open ? (
+<>
+  <div className="ml-[10rem] mt-[2rem]">
+  <button className="backbutton" onClick={()=> {setopen(false); setSelectedCourse(null); setSelectedDept(null)}}>back</button>
+  </div>
+     <Reseachdetails course={selectedCourse} data1 = {journalPublicationid[0]}/>
+
+  </>
+      ):(
+        <div className="research-journal-container">
         <h1 className="research-journal-title">
           Journal Publication
         </h1>
 
         <div className="course-selection-container">
           
-          {courses.map((course) => (
+          {dept.courses.map((course,id) => (
             <div
               key={course}
-              className={`course-card ${
-                selectedCourse === course ? "expanded" : ""
-              }`}
-              onClick={() => handleCourseClick(course)}
+              className={"course-card"}
+              onClick={() => handleCourseClick(course,dept.dept_id[id])}
             >
               <div className="course-header">
                 <span className="icon"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-book-open course-icon w-10 text-lg text-secd dark:text-drks"><path d="M12 7v14"></path><path d="M3 18a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1h5a4 4 0 0 1 4 4 4 4 0 0 1 4-4h5a1 1 0 0 1 1 1v13a1 1 0 0 1-1 1h-6a3 3 0 0 0-3 3 3 3 0 0 0-3-3z"></path></svg></span>
@@ -94,6 +122,8 @@ export default function Journalpublication({theme, toggle}) {
         </div>
 
       </div>
+      )}
+      
     </>
   );
 }
