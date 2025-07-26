f0660a87e9173970e653589aad8cbb9efa105e30
PraneshPK2005 <praneshvaradharaj@gmail.com>
2025-07-26 17:50:59 +0530

    rate limiter set in web team route

--- backend/main-backend/routes/web_team_routes.js
+++ backend/main-backend/routes/web_team_routes.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const { getWebTeamData } = require('../controllers/web_team_controllers');
+const createRateLimiter = require('../middlewares/ratelimiter');
+const limiter = createRateLimiter({ max: 20, windowMs: 5 * 60 * 1000 });
 
 const router = express.Router();
 
-router.get('/web_team',getWebTeamData);
-
+router.get('/web_team', limiter, getWebTeamData);
 module.exports = router
