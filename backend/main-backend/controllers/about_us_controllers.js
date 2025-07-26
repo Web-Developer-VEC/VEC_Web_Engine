69ca2ab31dd40f151508d896630a0209e5d19c82
Siddharth-magesh <siddharthmagesh007@gmail.com>
2025-07-26 14:33:56 +0000

    all those announcement varibles are removed , atleast what i found was

--- backend/main-backend/controllers/about_us_controllers.js
+++ backend/main-backend/controllers/about_us_controllers.js
@@ -6,15 +6,15 @@ async function getAbtUs (req, res) {
     const collection = db.collection('about_us');
 
     try {
-        const announcements = await collection.find({}).toArray();
-        if (announcements.length === 0) {
-            return res.status(404).json({ message: 'No announcements found' });
+        const about_us = await collection.find({}).toArray();
+        if (about_us.length === 0) {
+            return res.status(404).json({ message: 'No about us found' });
         }
-        res.status(200).json(announcements);
+        res.status(200).json(about_us);
     } catch (error) {
-        console.error('Error fetching announcements:', error);
-        await logError(req, error, 'Error fetching announcements', 500);
-        res.status(500).json({ error: 'Error fetching announcements' });
+        console.error('Error fetching about_us:', error);
+        await logError(req, error, 'Error fetching about us', 500);
+        res.status(500).json({ error: 'Error fetching about us' });
     }
 }
 
