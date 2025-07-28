52692089dbb09b77b0bbb28a8dfe29328a74410a
PraneshPK2005 <praneshvaradharaj@gmail.com>
2025-07-28 10:32:52 +0530

    backup

--- backend/main-backend/routes/feedback_routes.js
+++ backend/main-backend/routes/feedback_routes.js
@@ -1,12 +1,11 @@
 const express = require('express');
-const { submitFeedback } = require('../controllers/feedback_controller');
-const mongoSanitize = require('../middlewares/nosql');
-const commonSanitizer = require('../middlewares/sanitizers/common_sanitizer');
+const { submitFeedback } = require('../controllers/feedback_controllers');
+const nosql = require('../middlewares/sanitizers/nosql_injection');
 const createRateLimiter = require('../middlewares/ratelimiter');
 
 const router = express.Router();
 const limiter = createRateLimiter({ max: 10, windowMs: 60 * 1000 });
 
-router.post('/submit_feedback', limiter, mongoSanitize, commonSanitizer, submitFeedback);
+router.post('/submit_feedback', limiter, nosql, submitFeedback);
 
 module.exports = router;
