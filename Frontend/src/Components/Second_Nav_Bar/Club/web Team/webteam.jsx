89bdd3e263909711d9cb1159a5a817592119be22
Ajith <134156010+Ajith-ajay@users.noreply.github.com>
2025-07-31 23:53:56 +0530

    Updated wenteam.jsx (#292)
    * Some video Changes
    
    some video added in the public folder and accessed in the page banner
    
    * Some changes
    
    Some changes in landing and credit page design
    
    * Web team Page
    
    Web team Page created and integrated successfully
    
    * Load comp set
    
    Load comp set for iic incubation, ecell
    
    * New Backend Integrated
    
    * Link Path changed for regulation
    
    * Research Page integrated
    
    * Changes
    
    * Console log removed
    
    Console log removed
    
    * Some mobile desktop view fix
    
    * Landing page finished for lg screen
    
    * Developers page finished
    
    * webteam fix
    
    * Ratelimt set by ajith
    
    * Developers gone 😒
    
    * Update webteam.jsx
    
    * Update webteam urlparser
    
    * Updated wenteam.jsx

--- Frontend/src/Components/Second_Nav_Bar/Club/web Team/webteam.jsx
+++ Frontend/src/Components/Second_Nav_Bar/Club/web Team/webteam.jsx
@@ -48,7 +48,7 @@ const StaffCard = ({ member }) => {
     <div className="bg-prim dark:bg-drkp rounded-2xl shadow-md p-4 md:flex gap-10 w-[80vw] md:w-[40vw] lg:w-[30vw] items-center text-center border border-gray-100 dark:border-gray-700 ">
       <div className="w-28 h-28 md:w-32 md:h-32 mb-4 ml-[30%] md:ml-0 lg:ml-0">
         <img
-          src={member?.image}
+          src={UrlParser(member?.image)}
           alt={member?.name}
           className="w-full h-full object-cover rounded-full border-4 border-white dark:border-gray-800 shadow"
         />
