7f078c63d068d7a0cd216690b745ba76a6771155
4274-hari <hari.velusmy@gmail.com>
2025-08-10 00:26:16 +0530

    change in insertion

--- database/test.py
+++ database/test.py
@@ -14,34 +14,18 @@ from pymongo import MongoClient
 def insert_about_us():
     collection = db["about_us"]
 
-    with open("/VEC_Web_Engine/docs/about_us.json", "r", encoding="utf-8") as file:
+    with open("/root/VEC_Web_Engine/docs/about_us.json", "r", encoding="utf-8") as file:
         about_data = json.load(file)
 
         for section in about_data:
             section_type = section["type"]
             document = {
-                "type": section_type,
+                "section": section_type,
                 "data": section["data"]
             }
             collection.insert_one(document)
 
     print("about_us sections inserted successfully.")
 
-def insert_administration_sections():
-    collection = db["administration"]
 
-    with open("/VEC_Web_Engine/docs/administration.json", "r", encoding="utf-8") as file:
-        admin_data = json.load(file)
-
-        for section in admin_data:
-            section_type = section["type"]
-            document = {
-                "type": section_type,
-                "data": section["data"]
-            }
-            collection.insert_one(document)
-
-    print("Administration sections inserted successfully.")
-
-insert_administration_sections()
 insert_about_us()
