1971dc2ed994f0ffbcd737b982a1719688cd805a
vcash-21 <saivikaash@gmail.com>
2025-09-09 15:13:15 +0530

    Hostel admin page operations
    Hostel's admin page's works
    - Insert controller
    -Update controller
    -Delete controller
    -Handle controller
    -Hostel routes
    -role access models

--- Backend/backend/admin-backend/models/role_access_models.js
+++ Backend/backend/admin-backend/models/role_access_models.js
@@ -1,7 +1,8 @@
 // roleAccess.js
 const roleAccessMap = {
   gallery: ["super_admin", "gallery_admin"],
-  help_desk:["super_admin"]
+  help_desk:["super_admin"],
+  hostel_details:["hostel_admin","super_admin"]
   // add more collection → roles here
 };
 
