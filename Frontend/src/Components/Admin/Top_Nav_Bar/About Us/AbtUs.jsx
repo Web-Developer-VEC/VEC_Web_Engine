ab3697ee0db2f0507b707436aefd7a0624af417a
Dakshan B <dakshan195@gmail.com>
2026-02-04 10:52:43 +0530

    Update AbtUs.jsx

--- Frontend/src/Components/Admin/Top_Nav_Bar/About Us/AbtUs.jsx
+++ Frontend/src/Components/Admin/Top_Nav_Bar/About Us/AbtUs.jsx
@@ -682,7 +682,7 @@ const AdminAbtUs = ({ theme, toggle }) => {
                   onClick={handleCancel}
                   className="bg-gray-400 hover:bg-gray-500 text-white px-4 py-2 rounded flex items-center gap-2 shadow-lg"
                 >
-                  Cancel                </button>
+                  Cancel</button>
                 {changed && (
                   <button
                     onClick={handleSave}
