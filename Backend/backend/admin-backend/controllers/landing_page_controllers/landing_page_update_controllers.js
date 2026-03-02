8906e60f5da1947a3196c862769a9a63cc209448
Dharmesh1809 <dharmeshb2005@gmail.com>
2026-03-02 15:09:03 +0530

    changes

--- Backend/backend/admin-backend/controllers/landing_page_controllers/landing_page_update_controllers.js
+++ Backend/backend/admin-backend/controllers/landing_page_controllers/landing_page_update_controllers.js
@@ -45,7 +45,6 @@ async function updateData(tempDoc, mainCollection) {
 
             
             const new_data = Array.isArray(meta_data)?meta_data:[meta_data];
-console.log(new_data);
 
             await mainCollection.updateOne(
                 { type: collection_type },
