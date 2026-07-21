3ff5d155ddecdda5d0de40a2d7aa3c3b6380c5bd
Jasim-Saad-P-A <jasim860p@gmail.com>
2026-07-21 15:28:01 +0530

    hod_image -> image_path

--- Frontend/src/Components/Main/Top_Nav_Bar/Academics/sections/HeadDepartment.jsx
+++ Frontend/src/Components/Main/Top_Nav_Bar/Academics/sections/HeadDepartment.jsx
@@ -20,7 +20,7 @@ const HeadDepartment = ({ data }) => {
     Qualification = [hod_details?.[0]?.qualification],
     designation = hod_details?.[0]?.designation,
     Hod_message = hod_details?.[0]?.hod_message,
-    Image = hod_details?.[0]?.hod_image, // Provide a default path if needed
+    Image = hod_details?.[0]?.image_path, // Provide a default path if needed
     Social_media_links = hod_details?.[0]?.Social_media_links || {},
     resume = hod_details?.[0]?.pdf_path
   } = hod_details || {};
