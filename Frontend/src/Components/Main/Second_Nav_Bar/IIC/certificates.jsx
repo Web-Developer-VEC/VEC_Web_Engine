27d5cb01ac12a0e60c94a4934f8f777b5c7ee98c
Jasim-Saad-P-A <jasim860p@gmail.com>
2026-07-22 10:18:55 +0530

    Updates in infrastructure of academics. (#670)
    * modified Head.jsx and certificates.jsx
    
    * Changes in Main HeadDepartment
    
    updated urlparser
    
    * Updated Infrastructure in main
    
    changes in urlparser, added return statement in infrastructure_images map
    
    * Changes in head and Certificate
    
    * Static path changes in head
    * Style changes for boxes and view pdf in certificates
    
    * hod_image -> image_path

--- Frontend/src/Components/Main/Second_Nav_Bar/IIC/certificates.jsx
+++ Frontend/src/Components/Main/Second_Nav_Bar/IIC/certificates.jsx
@@ -1,62 +1,66 @@
 import { useState } from "react";
 import LoadComp from "../../LoadComp";
 
-export default function IicFacCertificate({data}) {
-    const [selectedAction, setSelectedAction] = useState(null)
-        const BASE_URL = process.env.REACT_APP_BASE_URL;
+export default function IicFacCertificate({ data }) {
+  const [selectedAction, setSelectedAction] = useState(null)
+  const BASE_URL = process.env.REACT_APP_BASE_URL;
 
-    const UrlParser = (path) => {
-        return path?.startsWith("http") ? path : `${BASE_URL}${path}`;
-    };
-        const openPdf = (category, name) => {
-        setSelectedAction({category, name})
-    }
-    
+  const UrlParser = (path) => {
+    return path?.startsWith("http") ? path : `${BASE_URL}${path}`;
+  };
+  const openPdf = (category, name) => {
+    setSelectedAction({ category, name })
+  }
 
-    const certificateArray = Array.isArray(data) ?
+
+  const certificateArray = Array.isArray(data) ?
     data?.map((name, index) => ({
-        name: name?.year,
-        path: UrlParser(name?.image_path),
+      name: name?.year,
+      path: UrlParser(name?.image_path),
     })) : []
-    
+
   return (
     <>
       {data ? (
         <div className="nirf-content mt-12">
-          <h2 className="text-accn dark:text-drkt text-center text-4xl mb-4 font-bold">IIC Certificate</h2>
-
+          <h2 className="text-accn dark:text-drkt text-center text-4xl mb-4 font-bold">
+            IIC Certificate
+          </h2>
           <div className="nirf-details dark:bg-[color-mix(in_srgb,theme(colors.drkp)_95%,white)] height">
-            <div className="nirf-year-actions faculty-icc flex flex-col md:flex-row text-center">
+
+
+
+            {/* Certificate Buttons */}
+            <div className="flex flex-wrap justify-center gap-8 my-10">
               {certificateArray.map((action, index) => {
                 const isActive =
                   selectedAction &&
                   selectedAction.category === "Certificate" &&
                   selectedAction.name === action.name;
-
                 return (
-                  
                   <div
                     key={index}
-                    className={`px-6 py-3 font-semibold text-center rounded-xl hover:bg-accn hover:text-prim dark:hover:bg-brwn
-                      ${
-                        isActive
-                          ? "bg-[#800000] text-white"
-                          : "bg-secd dark:bg-drks"
-                      }`}
                     onClick={() => openPdf("Certificate", action.name)}
+                    className={`w-52 h-16 rounded-xl flex items-center justify-center cursor-pointer font-bold text-lg shadow-lg transition-all duration-300 hover:-translate-y-1
+                    ${isActive
+                        ? "bg-[#800000] text-white"
+                        : "bg-[#FDCC03] text-black hover:bg-[#800000] hover:text-white"
+                      }
+                  `}
                   >
                     {action.name}
                   </div>
                 );
               })}
             </div>
-
+            {/* PDF Viewer */}
             {selectedAction && selectedAction.category === "Certificate" && (
-
               <div className="border p-8 mt-20 w-[94%] mx-auto bg-prim dark:bg-drkp shadow-lg">
-                <h3 className="text-center mb-2">{`Viewing: ${selectedAction.name}`}</h3>
-                <embed
+                <h3 className="text-center mb-2">
+                  Viewing: {selectedAction.name}
+                </h3>
 
+                <embed
                   className="embed"
                   src={
                     certificateArray.find(
@@ -66,8 +70,7 @@ export default function IicFacCertificate({data}) {
                   type="application/pdf"
                   width="100%"
                   height="600px"
-                /> 
-              
+                />
               </div>
             )}
           </div>
@@ -78,5 +81,5 @@ export default function IicFacCertificate({data}) {
         </div>
       )}
     </>
-  );
-}
+  )
+};
